ee157cbff543f80c7e5355ab56fcd1682b288a49
edo <72197345+alfredotanutama@users.noreply.github.com>
2021-05-16 13:22:19 +0700

    Add files via upload
    Kumpulan file yang dicreate saat masih berkuliah di Teknik Industri USU (Lab Komputasi)

---
+++ Luas Lingkaran/src/Luas/Luas_Lingkaran.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Luas;
+
+/**
+ *
+ * @author Alfredo Tanutama
+ */
+public class Luas_Lingkaran {
+    public static void main(String[] args) {
+        double pi, r, luas;
+        r= 10;
+                pi=3.14;
+                luas= pi*r*r;
+                System.out.print("Luas Lingkaran:");
+                System.out.println(luas);
+                        
+    }
+}
